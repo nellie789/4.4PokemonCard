2778df6e990c5299e04798597e29d4ecd06eae63
nellie789 <62j9@fresnou.org>
2022-12-07 11:35:46 -0800

    complete edit form

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -12,35 +12,69 @@ module.exports.renderEditForm = async function(req, res, next) {
     res.render('edit', {card});
 }
 
+module.exports.updateCard = async function(req,res) {
+    await Card.update(
+        {
+            name: req.body.name,
+            hp: req.body.hp,
+            image: req.body.image,
+            powerOne: req.body.powerOne,
+            powerOneDamage: req.body.powerOneDamage,
+            powerTwo: req.body.powerTwo,
+            powerTwoDamage: req.body.powerTwoDamage,
+            weakness: req.body.weakness,
+            resistance: req.body.resistance,
+            retreat: req.body.retreat
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
 
+module.exports.deleteCard = async function(req,res) {
+    await Card.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
 
+module.exports.renderAddForm = function(req, res){
+    const card = {
+        name: "",
+        hp: "",
+        image: "",
+        powerOne: "",
+        powerOneDamage: "",
+        powerTwo: "",
+        powerTwoDamage: "",
+        weakness: "",
+        resistance: "",
+        retreat: "",
+    };
+    res.render('add', {card});
+}
 
-// module.exports.viewAll = function(req, res, next) {
-//     const cards = [{
-//         id: 1,
-//         name: 'Pikachu',
-//         hp: '50 HP',
-//         image: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/09/Pokemon-Pikachu-18-different-types-fan-art.jpg',
-//         powerOne: 'Gnaw',
-//         powerOneDamage: 10,
-//         powerTwo: 'Lightning Bolt',
-//         powerTwoDamage: 50,
-//         weakness: 1,
-//         resistance: 1,
-//         retreat: 2
-//     },
-//         {
-//             id: 2,
-//             name: 'Shinx',
-//             hp: '60 HP',
-//             image: 'http://pm1.narvii.com/6457/b9a93881c381821d49076e86b0040e06ba19d9fb_00.jpg',
-//             powerOne: 'Rear Kick',
-//             powerOneDamage: 20,
-//             powerTwo: 'Static Shock',
-//             powerTwoDamage: 30,
-//             weakness: 1,
-//             resistance: 2,
-//             retreat: 1
-//         }];
-//     res.render('index', {cards});
-// }
+module.exports.addCard = async function(req, res) {
+    await Card.create(
+        {
+            name: req.body.name,
+            hp: req.body.hp,
+            image: req.body.image,
+            powerOne: req.body.powerOne,
+            powerOneDamage: req.body.powerOneDamage,
+            powerTwo: req.body.powerTwo,
+            powerTwoDamage: req.body.powerTwoDamage,
+            weakness: req.body.weakness,
+            resistance: req.body.resistance,
+            retreat: req.body.retreat
+        });
+    res.redirect('/');
+}
